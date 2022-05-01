705d06c9e7542ac23926562ad7cf89ca3f65c790
Efe Erdem <efeerdem2001@hotmail.com>
2022-05-01 20:32:46 +0300

    progress in crash-course

---
+++ src/components/Task.vue
@@ -0,0 +1,46 @@
+<template>
+  <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+    <h3>{{ task.text }}
+        <i @click="$emit('delete-task',task.id)" class="fas fa-times"></i>
+    </h3>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+  },
+};
+</script>
+
+<style scoped>
+.fas {
+    color: rgb(235, 0, 0);
+    cursor: pointer;
+    opacity: 0.75;
+}
+
+.fas:hover {
+    transform: scale(1.15);
+    opacity: 1;
+}
+
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+}
+
+.task.reminder {
+  border-left: 5px solid green;
+}
+
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
